2e1bc17217452981417aa97997b26b725b8f52d9
Ricardo Cavalcante <cavalcanter@gmail.com>
2019-10-08 18:33:33 -0300

    Adicionando controle de dados usando H2

--- src/main/java/com/ricardocavalcante/cadastro/repository/RebeldeRepository.java
+++ src/main/java/com/ricardocavalcante/cadastro/repository/RebeldeRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface RebeldeRepository extends JpaRepository<Rebelde, Long> {
 
-    @Query("SELECT R.* FROM rebelde AS R INNER JOIN denuncias AS D ON D.traidor = R.id GROUP BY R.id WHERE COUNT(D.traidor) >= 3 ")
-    List<Rebelde> findAllTraidores();
+    //@Query("SELECT R.* FROM rebelde AS R INNER JOIN denuncias AS D ON D.traidor = R.id GROUP BY R.id WHERE COUNT(D.traidor) >= 3 ")
+    //List<Rebelde> findAllTraidores();
 
 }
